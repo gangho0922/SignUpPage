5756374eda661615afe553dbdd0a16e3f26e420c
AnnKangHo <duddnd6897@gmail.com>
2022-10-18 20:54:17 +0900

    Initial Commit

---
+++ SignUpPage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignUpPage
+//
+//  Created by AnnKangHo on 2022/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SignUpPage/SignUpPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpPageApp.swift
+//  SignUpPage
+//
+//  Created by AnnKangHo on 2022/10/18.
+//
+
+import SwiftUI
+
+@main
+struct SignUpPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
